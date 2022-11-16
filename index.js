369df21c39da0110af2cf5c1b0bb69c357c70006
Joshua <janorris65@gmail.com>
2022-11-16 00:14:12 -0600

    add forecast introfunc

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+let exStlForecastWeather =
+  "http://api.openweathermap.org/data/2.5/forecast?lat=38&lon=-89&appid=5082e4062959ff23200dac304c5cf020";
+
+function getWeatherForecast() {
+  fetch(exStlForecastWeather)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var cityStlNoontemp1 = document.createElement("h3");
+      var cityStlNoontemp2 = document.createElement("h3");
+      var position = document.querySelector("h2");
+      cityStlNoontemp1.textContent =
+        "One Day Forecast " + data.list[2].main.temp;
+      cityStlNoontemp2.textContent =
+        "Two Day Forecast " + data.list[9].main.temp;
+      position.appendChild(cityStlNoontemp1);
+      position.appendChild(cityStlNoontemp2);
+    });
+}
+
 let exStlWeather =
   "http://api.openweathermap.org/data/2.5/weather?q=63116,US&APPID=5082e4062959ff23200dac304c5cf020";
 
@@ -11,6 +33,7 @@ function getWeather() {
       var cityStl = document.createElement("h3");
       var weatherStl = document.createElement("p");
       var tempStl = document.createElement("p");
+
       var position = document.querySelector("h1");
       cityStl.textContent = data.name;
       weatherStl.textContent = data.weather[0].main;
@@ -21,3 +44,4 @@ function getWeather() {
     });
 }
 getWeather();
+getWeatherForecast();
---
+++ readme.md
@@ -0,0 +1,15 @@
+## Geo
+
+http://api.openweathermap.org/geo/1.0/direct?q=Ruma,IL,US&limit=5&appid=5082e4062959ff23200dac304c5cf020
+
+## forecast
+
+api.openweathermap.org/data/2.5/forecast?lat=38&lon=-89&appid=5082e4062959ff23200dac304c5cf020
+
+the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+## current
+
+http://api.openweathermap.org/data/2.5/weather?q=63116,US&APPID=5082e4062959ff23200dac304c5cf020
+
+city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
